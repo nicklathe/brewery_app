14ba0bb13daab8d77569e0bce4728613e029a444
Nick Lathe <nicklathe@icloud.com>
2014-12-16 16:18:53 -0800

    Added a bunch of functionality. App works, and is half styled, just need too try adding routing

---
+++ models/user.js
@@ -0,0 +1,48 @@
+"use strict";
+
+var bcrypt = require("bcrypt");
+
+module.exports = function(sequelize, DataTypes) {
+  var user = sequelize.define("user", {
+    name: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: "Please enter a valid email"
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [5,20],
+          msg: "Please enter a password between 5 to 20 characters long"
+        }
+      }
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+        models.user.hasMany(models.favorite);
+      }
+    },
+    hooks: {
+      beforeCreate: function(data, stuff, sendback){
+        var pwdToEncrypt = data.password;
+        var encryptedPwd = "placeholder";
+
+        bcrypt.hash(pwdToEncrypt, 10, function(err, hash){
+          encryptedPwd = hash;
+          data.password = hash;
+          sendback(null, data);
+        });
+      }
+    }
+  });
+
+  return user;
+};
--- public/script.js
+++ public/script.js
@@ -1,11 +1,11 @@
-// <script src='https://api.tiles.mapbox.com/mapbox.js/v2.1.4/mapbox.js'></script>
-
-// <script>
-// L.mapbox.accessToken = 'pk.eyJ1Ijoibmlja2xhdGhlIiwiYSI6InhteXZpY1kifQ.7auBYdWGL5ttVM1kbrn93Q';
-// var map = L.mapbox.map('map', 'nicklathe.506fa65a', {
-// 	scrollWheelZoom: false
-// }).setView({
-//     	center: [122.3070, 47.6357], 
-//     	zoom: 13
-//     });
-// </script>
+// $(function() {
+// 	$("#map").on("click", ".addButton", function(el) {
+// 		console.log(el);
+// 		alert("clicked");
+// 		var button = $(this);
+// 		el.preventDefault();
+// 		$.post("/list", {
+// 			name: button.data("name")
+// 		});
+// 	});
+// });
