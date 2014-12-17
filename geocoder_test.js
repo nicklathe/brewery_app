1d6a219e3645b07bdd6398b92f9ba10f2d872b3d
Nick Lathe <nicklathe@icloud.com>
2014-12-16 16:18:53 -0800

    Added a bunch of functionality. App works, and is half styled, just need too try adding routing

--- geocoder_test.js
+++
@@ -1,8 +0,0 @@
-var geocoder = require("geocoder");
-
-
-geocoder.geocode("eifel tower, paris, france", function ( err, data ) {
-  // do something with data
-  console.log(data.results[0].geometry);
-});
-
--- index.js
+++ index.js
@@ -5,76 +5,167 @@ var app = express();
 var BreweryDb = require("brewerydb-node");
 var brewdb = new BreweryDb(process.env.brew_db);
 var request = require("request");
+var flash = require("connect-flash");
+var bcrypt = require("bcrypt");
+var db = require("./models");
 
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(session({
-	secret: "ilovebeer",
+	secret: "ilovebeerandbreweries",
 	resave: false,
 	saveUninitialized: true
 }));
 
-// app.get("/", function(req, res){
-// 	request("https://maps.googleapis.com/maps/api/geocode/json?address=1050+North+34th+Street,+Seattle,+WA&key=AIzaSyAM7-sHXiFxjPvCruNLY_yikgBYOpzDVUM",
-// 	function(error, response, body){
-// 		res.send(body);
-// 	});
+app.use(flash());
+
+app.use(function(req, res, next){
+	req.getUser = function(){
+		return req.session.user || false;
+	};
+	next();
+});
+
+// Passes alert object to all pages with the wildcard * and res.locals
+app.get("*", function(req, res, next){
+	var alerts = req.flash();
+	res.locals.alerts = alerts;
+	next();
+});
+
+// app.get("/header", function(req, res){
+// 	var user = req.getUser();
+// 	res.render("header", {user:user});
 // });
 
-// app.get("/", function(req, res){
-// 	request("http://api.tiles.mapbox.com/v4/geocode/mapbox.places/1050+North+34th+Street+,Seattle,+WA.json?access_token=pk.eyJ1Ijoibmlja2xhdGhlIiwiYSI6InhteXZpY1kifQ.7auBYdWGL5ttVM1kbrn93Q",
-// 	function(err, response, body){
-// 		var result = JSON.parse(body);
-// 		res.send(result);
-// 	});
-// });
-
-
+// Inital request to get all the breweries from brewerydb and render them to the home page
 app.get("/", function(req, res){
-
+	var user = req.getUser();
 	request("http://api.brewerydb.com/v2/locations?key=d89bf914be5d33893a9cbe1cdd88556c&locality=seattle", function(err, response, body){
-		// console.log(body);
-		// res.send(body);
-		res.render("index", {mapData: body});
-	});
-	
-
-});
-
-// app.get("/", function(req, res){
-// 	res.render("header");
-// });
-
-// app.get("/", function(req, res){
-// 	brewdb.search.breweries({q: "rock bottom", withLocations: "Y"}, function(err, data){
-// 		res.send(data);
-// 	});
-	
-	// brewdb.search.beers({q: "fremont", withBreweries: "Y"}, function(err, data){
-	// 	res.send(data);
-	// });
-
-// });
-
-// app.get("/", function(req, res){
-// 	brewdb.breweries.find({locations: {locality: "Seattle"}}, function(err, data){
-// 		console.log(data);
-// 		res.send(data);
-// 	});
-// });
-
-
-// http://api.brewerydb.com/v2/breweries?name=*rock%20bottom*&key=d89bf914be5d33893a9cbe1cdd88556c
-
-app.get("/", function(req, res){
-	request("http://api.brewerydb.com/v2/breweries/name/search?q=Fremont&key=d89bf914be5d33893a9cbe1cdd88556c&format=json&withLocation=Y", function(err, response, body){
-		console.log(body);
-		res.send(body);
+		res.render("index", {mapData: body, user: user});
 	});
 });
 
+// Get's brewery individual info page. Currently can't access the right info on the ejs side.
+app.get("/brewery/:id", function(req, res){
+	var user = req.getUser();
+	var brewId = req.params.id;
+	// console.log(brewId);
+	request("http://api.brewerydb.com/v2/breweries?key=d89bf914be5d33893a9cbe1cdd88556c&ids=" + brewId, function(err, response, body){
+		// console.log(typeof breweryIndiv);
+		var breweryIndiv = JSON.parse(body);
+		// res.send(breweryIndiv);
+		res.render("brewery", {breweryIndiv:breweryIndiv, user:user});
+	});
+});
+
+// Posts breweries to list/db from pop up bubles on map
+app.post("/list", function(req, res){
+	var user = req.getUser();
+	db.favorite.findOrCreate({where: {brewery_name: req.body.brewery_name, userId: user.id}}).spread(function(savedBrewery, created){
+		if(created){
+			req.flash("success","Added to your list");
+			res.redirect("/");
+		} else {
+			req.flash("danger","Brewery already exists in your list");
+			res.redirect("/");
+		};
+	});
+});
+
+// Get's the list/My Breweries page
+app.get("/list", function(req, res){
+	var user = req.getUser();
+	if(user){
+		db.user.find({where: {id: user.id}}).then(function(userReturn){
+			db.favorite.findAll({where: {userId: userReturn.id}}).then(function(returnedList){
+				res.render("list", {returnedList:returnedList, user:user});
+				// res.send(returnedList);
+			});
+		});
+	} else {
+		req.flash("warning","Please login");
+		res.redirect("/");
+	};
+});
+
+// Get's the login/signup page
+app.get("/login", function(req, res){
+	var user = req.getUser();
+	res.render("login", {user:user});
+});
+
+// Creates new user in users model, and keeps user logged in via session
+app.post("/signup", function(req, res){
+	db.user.findOrCreate({where: {email: req.body.email},
+		defaults: {name: req.body.name, email: req.body.email, password: req.body.password}}).spread(function(createdUser, created){
+			req.session.user = {
+				id: createdUser.id,
+				name: createdUser.name,
+				email: createdUser.email
+			};
+			res.redirect("/");
+		}).catch(function(error){
+			if(error && Array.isArray(error.errors)){
+				error.errors.forEach(function(errorItem){
+					req.flash("danger", errorItem.message);
+					// res.send(errorItem);
+				});
+			} else {
+				req.flash("danger", "Unknown error");
+			}
+			res.redirect("/login");
+		});
+});
+
+// User login. Currently, doesn't display user name. Is displaying Unknown
+app.post("/login", function(req, res){
+	db.user.find({where: {email: req.body.email}}).then(function(userObj){
+		if(userObj){
+			bcrypt.compare(req.body.password, userObj.password, function(err, match){
+				if(match === true){
+					req.session.user = {
+						id: userObj.id,
+						email: userObj.email,
+						name: userObj.name
+					};
+					res.redirect("/");
+				} else {
+					req.flash("warning", "Invalid username or password");
+					res.redirect("/login");
+				}
+			});
+		} else {
+			req.flash("warning", "Unknown user.");
+			res.redirect("/login");
+		}
+	});
+});
+
+app.get("/logout", function(req, res){
+	delete req.session.user;
+	req.flash("info","You have been logged out");
+	res.redirect("/");
+});
+
+
+// Testing routes below
+
+app.get("/route", function(req, res){
+	var user = req.getUser();
+	request("http://api.brewerydb.com/v2/locations?key=d89bf914be5d33893a9cbe1cdd88556c&locality=seattle", function(err, response, body){
+		res.render("route", {mapData: body, user: user});
+	});
+});
 
 
 app.listen(3000);
+
+
+
+
+
+
+
