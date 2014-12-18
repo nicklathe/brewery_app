e865068abf1edb172f9bffb8e215c66059f3dde2
Nick Lathe <nicklathe@icloud.com>
2014-12-18 15:15:59 -0800

    Final commit before first Heroku deployment

--- public/script.js
+++ public/script.js
@@ -1,11 +1,16 @@
-// $(function() {
-// 	$("#map").on("click", ".addButton", function(el) {
-// 		console.log(el);
-// 		alert("clicked");
-// 		var button = $(this);
-// 		el.preventDefault();
-// 		$.post("/list", {
-// 			name: button.data("name")
-// 		});
-// 	});
-// });
+$(function() {
+	$("#map").on("click", ".addButton", function(event) {
+		// alert("clicked");
+		event.preventDefault();
+		var button = $(this);
+		$.post("/list", {
+			brewery_name: button.data("name"),
+			brewery_id: button.data("id")
+		});
+	});
+
+	$(".scroll_down").on("click", function(){
+		$("html, body").animate({scrollTop: $(document).height()}, 2000);
+		return false;
+	});
+});
